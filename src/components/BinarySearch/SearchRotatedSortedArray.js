bd196b4aac9ae2326c496c9572901867831ec14b
James Shoemake <jshoemake@howard.com>
2022-07-14 12:48:08 -0500

    added another case

--- src/components/BinarySearch/SearchRotatedSortedArray.js
+++ src/components/BinarySearch/SearchRotatedSortedArray.js
@@ -1,7 +1,37 @@
 import React from 'react'
 
 const SearchRotatedSortedArray = () => {
-  return <div>SearchRotatedSortedArray</div>
+  const searchArray = (arr, target) => {
+    let left = 0
+    let right = arr.length
+    let mid = 0
+
+    console.log(left, right, mid)
+
+    while (left <= right) {
+      mid = Math.floor((right + left) / 2)
+      if (arr[mid] === target) return mid
+
+      // if left
+      if (arr[left] <= arr[mid]) {
+        if ((target > arr[mid]) | (target < arr[left])) {
+          left = mid + 1
+        } else {
+          right = mid - 1
+        }
+      } else {
+        if ((target < arr[mid]) | (target > arr[right])) {
+          right = mid - 1
+        } else {
+          left = mid + 1
+        }
+      }
+    }
+
+    return -1
+  }
+
+  return <div>{searchArray([4, 5, 6, 7, 0, 1, 2], 5)}</div>
 }
 
 export default SearchRotatedSortedArray
