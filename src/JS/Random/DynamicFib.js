8721fd3bc87691d8fb0210559e8185c7b1e31c4e
James Shoemake <james.shoemake1@gmail.com>
2022-08-30 15:46:39 -0500

    Added a dynamic Fib solution.

---
+++ src/JS/Random/DynamicFib.js
@@ -0,0 +1,17 @@
+function fib(n, memo = {}) {
+  let result
+
+  if (memo[n] !== undefined) return memo[n]
+
+  if (n === 1 || n === 2) {
+    result = 1
+  } else {
+    result = fib(n - 1) + fib(n - 2)
+  }
+  memo[n] = result
+  return result
+}
+
+// console.log('command args',process.argv)
+let fibNum = process.argv[2]
+console.log(`fib(${fibNum})`, fib(fibNum))
